20f9cb47c7d07fb116f92db68a5274f5314a7ad5
jsyeo97 <jsyeo97@naver.com>
2023-07-30 21:21:10 +0900

    feat: 검증1 - Validation 구현

--- 스프링 MVC/validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
+++ 스프링 MVC/validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -1,2 +1,37 @@
-package hello.itemservice.validation;public class MessageCodesResolverTest {
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCOdesResolverObject() {
+
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+    }
+
 }
